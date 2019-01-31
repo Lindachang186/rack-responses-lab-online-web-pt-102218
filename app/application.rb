617b8d0446a18bdeec215292bac8637dfb3e0989
Linda Chang <lindachang186@gmail.com>
2019-01-31 22:24:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
     time = Time.new
 
-    if Time.hour > 12
+    if time.hour > 12
       resp.write "Good Morning!"
     else resp.write "Good Afternoon!"
     end
