e737be054b44bce94d8f2083a23adb974d153608
Linda Chang <lindachang186@gmail.com>
2019-01-31 22:21:38 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  time = Time.new
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if time.hour > 12
+      resp.write "Good Morning!"
+    else resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
